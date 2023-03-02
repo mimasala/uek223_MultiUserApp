83483e4b47212645bcd5adbfbd9c8e3f6b127cb8
mimasala <65661176+mimasala@users.noreply.github.com>
2023-03-02 11:10:46 +0100

    tests

--- src/components/pages/AdminDashboardHomePage/AdminDashboardHomePage.tsx
+++ src/components/pages/AdminDashboardHomePage/AdminDashboardHomePage.tsx
@@ -12,9 +12,9 @@ const AdminDashboardHomePage = () => {
         <Box sx={{ bgcolor: '#cfe8fc', height: '90vh', textAlign: 'center',  overflow: 'auto', marginBottom: "20%"}} >
             <h1>Admin Dashboard</h1>
             <Container maxWidth="md" >
-              <Link href="/admin/users">Users</Link>
+              <Link id="manageusers" href="/admin/users">Manage Users</Link>
               <br/>
-              <Link href="/admin/events">Events</Link>
+              <Link id="manageevents" href="/admin/events">Manage Events</Link>
             </Container>
         </Box>
       </Container>
